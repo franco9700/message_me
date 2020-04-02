4701403ab740794ab4961aa8f961fdd8b16efa80
franco9700 <francisco.iram97@gmail.com>
2020-04-01 17:40:47 -0700

    Create user and message controller, model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "franco", password: "huevos1")
+User.create(username: "rocio", password: "huevos1")
